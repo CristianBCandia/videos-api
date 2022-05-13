a0a0eeac33125af4c89620a5aa896d5e2c199a88
CristianBittencourt <cristian@omd.com.br>
2022-05-12 23:19:11 -0300

    Estrutura básica do projeto criada

---
+++ src/test/java/com/api/youtube/videos/VideosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.youtube.videos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
