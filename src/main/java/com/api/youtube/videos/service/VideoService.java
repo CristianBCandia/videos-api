5c3e1e02332e1537eb7b60c97e752ab63f6683fa
CristianBittencourt <cristian@omd.com.br>
2022-05-14 21:48:35 -0300

    deploy heroku

--- src/main/java/com/api/youtube/videos/service/VideoService.java
+++ src/main/java/com/api/youtube/videos/service/VideoService.java
@@ -12,7 +12,6 @@ import com.api.youtube.videos.dto.youtube.YoutubeVideosResponse;
 import com.api.youtube.videos.exception.CustomServerErrorException;
 import com.api.youtube.videos.exception.VideoNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 
